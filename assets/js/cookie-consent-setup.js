cd37b2a12e6e655d0a5c6557809d2e945f6ac2b8
idanbiran <62839448+idanbiran@users.noreply.github.com>
2026-03-22 20:35:20 +0000

    Deploying to gh-pages from @ idanbiran/biranlab.github.io@288ed018bb80865d37dca8afb38e29b7cb19e3c4 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://biranlab.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://idanbiran.github.io/biranlab.github.io/#contact">contact us</a>.'
               }
             ]
           }
